8f383f466679d200ee3b978cad301b9b9516d29c
Raul <raul.borda@binamics.com.ar>
2024-08-10 14:35:25 -0300

    agregado EP generate-totp

---
+++ index.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import speakeasy from 'speakeasy';
+import qrcode from 'qrcode';
+import qrcodeTerminal from 'qrcode-terminal';
+
+const app = express();
+app.use(express.json());
+
+let secret; 
+
+app.get('/generate-qr', (req, res) => {
+  secret = speakeasy.generateSecret({ length: 20 });
+  console.log(secret);
+  const otpauthUrl = speakeasy.otpauthURL({
+    secret: secret.base32,
+    label: 'app:johndoe@gmail.com', 
+    issuer: 'empresa', 
+    encoding: 'base32'
+  });
+
+  qrcodeTerminal.generate(otpauthUrl, { small: true }, function (qrcode) {
+    console.log('QR');
+    console.log(qrcode);
+  });
+
+  qrcode.toDataURL(otpauthUrl, (err, data_url) => {
+    if (err) {
+      res.status(500).json({ error: 'Error generando QR' });
+    } else {
+      res.json({ secret: secret.base32, qrcode: data_url });
+    }
+  });
+});
+
+app.post('/verify-totp', (req, res) => {
+  const { token } = req.body;
+
+  if (!secret) {
+    return res.status(400).send('Secret no definido. Generar QR primero.');
+  }
+
+  const verified = speakeasy.totp.verify({
+    secret: secret.base32,
+    encoding: 'base32',
+    token: token
+  });
+
+  if (verified) {
+    res.send('🤙🏼🤙🏼🤙🏼🤙🏼');
+  } else {
+    res.status(400).send('👎🏼👎🏼👎🏼👎🏼');
+  }
+});
+
+app.get('/generate-totp', (req, res) => {
+    if (!secret) {
+      return res.status(400).send('Secret no definido. Generar QR primero.');
+    }
+    const token = speakeasy.totp({ secret: secret.base32, encoding: 'base32' });
+    res.json({ token });
+  });
+
+app.listen(3000, () => console.log('Server en port 3000...'));
